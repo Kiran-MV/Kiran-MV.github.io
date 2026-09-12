a06c261020c9d5336d676843dfee2df359e71657
Kiran MV <kmv@volvocars.com>
2026-09-12 13:24:33 +0200

    Connect birthday RSVP form

--- birthday-rsvp-invite/script.js
+++ birthday-rsvp-invite/script.js
@@ -1,10 +1,15 @@
-const RSVP_ENDPOINT = "";
+const RSVP_ENDPOINT = "https://formsubmit.co/kiranmutturu@gmail.com";
 
 const form = document.querySelector("#rsvp-form");
 const note = document.querySelector("#form-note");
 
 if (RSVP_ENDPOINT) {
   form.action = RSVP_ENDPOINT;
+  const nextPage = document.createElement("input");
+  nextPage.type = "hidden";
+  nextPage.name = "_next";
+  nextPage.value = new URL("thank-you.html", window.location.href).href;
+  form.append(nextPage);
   note.textContent = "Your RSVP will be sent securely when you submit this form.";
 } else {
   form.addEventListener("submit", (event) => {
